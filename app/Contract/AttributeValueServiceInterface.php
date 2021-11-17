13e6e7be8eac36b72b27c373325dd39378a8d6d5
liuxzhi <15864375581@163.com>
2021-11-17 15:52:27 +0800

    AttributeValueServiceInterface格式化

--- app/Contract/AttributeValueServiceInterface.php
+++ app/Contract/AttributeValueServiceInterface.php
@@ -5,5 +5,11 @@ namespace App\Contract;
 
 interface AttributeValueServiceInterface
 {
-
+    /**
+     * 根据查询条件获取属性值
+     * @param array $conditions
+     * @param array $options
+     * @return array
+     */
+    public function getAttributeValueListByCondition($conditions=[], $options=[]) : array ;
 }
