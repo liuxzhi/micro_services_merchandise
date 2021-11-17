cf7d26bb1806968dff44627fa5ae7d2b43a66fab
liuxzhi <15864375581@163.com>
2021-11-17 18:42:14 +0800

    新增商品id

--- app/Model/MerchandiseAttribute.php
+++ app/Model/MerchandiseAttribute.php
@@ -25,13 +25,13 @@ class MerchandiseAttribute extends Model
      *
      * @var array
      */
-    protected $fillable = ['id', 'attribute_id', 'is_prime', 'state', 'deleted_at', 'updated_at', 'created_at'];
+    protected $fillable = ['id', 'merchandise_id','attribute_id', 'is_prime', 'state', 'deleted_at', 'updated_at', 'created_at'];
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'attribute_id' => 'integer', 'is_prime' => 'integer', 'state' => 'integer', 'created_at' => 'integer', 'updated_at' => 'integer', 'deleted_at' => 'integer'];
+    protected $casts = ['id' => 'integer', 'merchandise_id' => 'integer', 'attribute_id' => 'integer', 'is_prime' => 'integer', 'state' => 'integer', 'created_at' => 'integer', 'updated_at' => 'integer', 'deleted_at' => 'integer'];
 
     protected $dateFormat = 'U';
 }
