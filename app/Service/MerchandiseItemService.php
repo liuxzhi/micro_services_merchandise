e8acc9a389b585c1723630c02207715ab2c2b83f
liuxzhi <15864375581@163.com>
2021-11-17 11:01:49 +0800

    代码格式化

--- app/Service/MerchandiseItemService.php
+++ app/Service/MerchandiseItemService.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use App\Contract\MerchandiseItemAttributeServiceInterface;
+use App\Contract\MerchandiseItemServiceInterface;
 
-class MerchandiseItemService extends AbstractService implements MerchandiseItemAttributeServiceInterface
+class MerchandiseItemService extends AbstractService implements MerchandiseItemServiceInterface
 {
     public function __construct(){
         parent::__construct();
