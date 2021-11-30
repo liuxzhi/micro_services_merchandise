de919474c4e6cccc705e7ce767a3263cad88b3c4
liuxzhi <15864375581@163.com>
2021-11-30 15:36:44 +0800

    设置商品添加验证规则

--- app/Request/Merchandise/MerchandiseSceneRequest.php
+++ app/Request/Merchandise/MerchandiseSceneRequest.php
@@ -11,7 +11,13 @@ trait MerchandiseSceneRequest
      */
     protected function scene(): array
     {
-        return [];
+        return [
+            'create' => ['introduction', 'name', 'item_attribute_value', 'items'],
+            'update' => ['id', 'introduction', 'name', 'item_attribute_value', 'items'],
+            'get' => ['id'],
+            'state' => ['id'],
+            'itemState' => ['id'],
+        ];
     }
 
     /**
@@ -19,7 +25,13 @@ trait MerchandiseSceneRequest
      */
     protected function rules(): array
     {
-        return [];
+        return [
+            'id' => 'required|integer|min:1|bail',
+            'name' => 'required|string|min:1|bail',
+            'introduction' => 'required|string|bail',
+            'item_attribute_value' => 'required|array|bail',
+            'items' => 'required|array|bail',
+        ];
     }
 
     /**
@@ -27,7 +39,19 @@ trait MerchandiseSceneRequest
      */
     protected function messages(): array
     {
-        return [];
+        return [
+            'id.required' => 'id 必填',
+            'id.integer' => 'id 必需为整数',
+            'name.required' => 'name 必填',
+            'name.min' => 'name 不可为空',
+            'name.string' => 'name 必需是字符串类型',
+            'introduction.required' => 'introduction 必填',
+            'introduction.string' => 'string 必需是字符串类型',
+            'item_attribute_value' => 'item_attribute_value 必填',
+            'item_attribute_value.array' => 'item_attribute_value 必需是数组',
+            'items' => 'items 必填',
+            'items.array' => 'array 必需是数组',
+        ];
     }
 
     /**
